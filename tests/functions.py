0f7c731b46adab7ff736aade902dceafaccef02a
Boitshepo Masemola <boitshepo.masemola@gmail.com>
2019-10-29 12:35:26 +0200

    created my calculator and tests

---
+++ tests/functions.py
@@ -0,0 +1,11 @@
+def adding(*args):
+    add = 0
+    for a in args:
+        add += a
+    return add
+
+def multiplying(*args):
+    multiply = 1
+    for b in args:
+        multiply *= b
+    return multiply 
