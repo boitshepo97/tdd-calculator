bfe5f685733d5500f1769546ae320c78367acc28
Boitshepo Masemola <54136739+boitshepo97@users.noreply.github.com>
2019-11-04 11:30:43 +0200

    changed function names to required names.

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,25 +1,25 @@
 from functions import adding, multiplying
 
 def test_add_zeros():
-    result = adding(0,0)
+    result = add(0,0)
     assert result == 0
 
 def test_add_two():
-    result = adding(4,5)
+    result = add(4,5)
     assert result == 9
 
 def test_add_negatives():
-    result = adding(-1,-1)
+    result = add(-1,-1)
     assert result == -2
 
 def test_add_multiples():
-    result = adding(1,2,3,4)
+    result = add(1,2,3,4)
     assert result == 10
 
 def test_multiply_two():
-    result = multiplying(1,2)
+    result = multiply(1,2)
     assert result == 2
 
 def test_multiply_multiples():
-    result = multiplying(1,2,3,4)
+    result = multiply(1,2,3,4)
     assert result == 24
